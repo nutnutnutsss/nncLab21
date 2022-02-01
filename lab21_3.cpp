95efbe6e7cfa0dfba9e9fc89a06bfcdfde5a0379
NutNatcha <natcha_me@cmu.ac.th>
2022-02-01 21:16:36 +0700

    lab21_3 complete

--- lab21_3.cpp
+++ lab21_3.cpp
@@ -8,9 +8,10 @@
 using namespace std;
 
 struct student{
-
-    //Define struct student with four member (name ,id , gender, gpa);
-    
+	char *name;
+	int id;
+	char gender;
+	float gpa;
 };
 
 struct course{
@@ -20,7 +21,7 @@ struct course{
 	vector<student *> student_list;
 };
 
-student * findstudent(vector<student> allstudents,int key){ //There is something wrong in this line.
+student * findstudent(vector<student> &allstudents,int key){
 	for(unsigned int i = 0; i < allstudents.size(); i++){
 		if(allstudents[i].id  == key) return &allstudents[i];
 	}
@@ -60,16 +61,22 @@ int main(){
 	vector<course> allcourses;
 	
 	string textline;
+	char names[100][100];
+	int i = 0;
 	
 	while(getline(student_file,textline)){
 		student s; 
-		
-		//Use sscanf() to split the values in textline and assign those values to the members of struct s;
-
-		allstudents.push_back(s); 		
+		char format[] = "%[^,],%d,%c,%f";
+		int ID;
+		char sex;
+		float grade;
+		sscanf(textline.c_str(),format,&names[i][0],&ID,&sex,&grade);
+		s = {&names[i][0],ID,sex,grade};
+		i++;
+		allstudents.push_back(s);
 	}
 	
-	int state = 1;
+	int state = 1,count = 0, N = 0;
 	while(getline(course_file,textline)){
 		if(state == 1){
 			course c;
@@ -83,18 +90,22 @@ int main(){
 			if(textline == "> Students"){
 				state = 3;
 			}else{
-			
-			    //Append (push_back) textline to lecture_list[] of the recently added course in allcourses[];
-			    
+				do{
+					if(textline == "> Students"){
+						count++;
+						state = 3;
+						break;
+					}
+					allcourses[count].lecture_list.push_back(textline);
+				}while(getline(course_file,textline));			    
 			}			
 		}else{
 			if(textline == "---------------------------------------"){
 				state = 1;
+				N++;
 			}else{
 				student *p = findstudent(allstudents,atof(textline.c_str()));
-				
-				//Append (push_back) p to student_list of the recently added course in allcourses[];
-				
+				allcourses[N].student_list.push_back(p);				
 			}
 		}
 	}
